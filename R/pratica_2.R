5020229c985835c572d778fa61d64656c42a0a2a
roinacio <roinacio@santacasasaudesjc.com.br>
2021-08-26 22:41:10 -0300

    PRATICA EM R - FUNÇÕES

---
+++ R/pratica_2.R
@@ -0,0 +1,12 @@
+##Trabalhando com funções
+
+getwd()
+#sem parentes
+sd
+
+##Função Head retorna as primeiras linhas de uma data frame head(num = obj, n = numero de linhas)
+
+head(x=iris, n=2)
+head(iris)
+head(iris,2)
+head(n=22)
