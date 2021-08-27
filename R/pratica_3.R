4dd4a1b690c5c0ed6851a8f01d51c37c0e6f35b2
roinacio <roinacio@santacasasaudesjc.com.br>
2021-08-26 22:52:17 -0300

    PRATICA EM R - HEAD / TAIL / DIM / FILE.CHOOSE

---
+++ R/pratica_3.R
@@ -0,0 +1,40 @@
+##Principais Funcoes
+##Head e Tail
+
+esoph ##Base teste
+
+##Defaut 6 Linhas
+head(esoph) ## Cabeçalho
+
+tail(esoph) ## Ultimas Linhas
+
+##10 Linhas
+head(esoph, n=10) ## 10 primeiras linhas
+
+##Dimensoes
+dim(esoph) ## Numero de linhas e Colunas
+
+dim(esoph)[1] ##Acessar primeiro conteudo do vetor
+
+##Comprimento
+length(islands) ##retorna o tamanho
+
+##Nome das colunas
+
+colnames(esoph) ##colunas
+rownames(esoph) ##linhas
+
+summary(esoph) ##Resumo dos dados de forma categorica, retornando o balanceamento de cada categoria
+
+##Escolher arquivos
+
+x = file.choose()
+x
+
+
+
+
+
+
+
+
